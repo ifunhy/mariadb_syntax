d39c606089b7c42790d3a7249649a4113b7bb374
ifunhy <guswl8296@gmail.com>
2025-05-22 17:02:55 +0900

    0522

---
+++ 1.DDL.sql
@@ -0,0 +1,69 @@
+-- mariadb 서버에 접속
+mariadb -u root -p -- 입력 후 비밀번호 별도 입력
+
+-- 스키마(database) 생성
+create database 스키마명;
+
+-- 스키마 삭제
+drop database 스키마명;
+
+-- 스키마 목록 조회
+show databases;
+
+-- 스키마 선택
+use 스키마명;
+
+-- 문자 인코딩 변경
+alter database 스키마명 default character set = utf8mb4;    -- mb4 이모지 같은 추가된 문자
+
+-- 문자 인코딩 조회
+show variables like 'charater_set_server';
+
+-- 테이블 생성
+create table 테이블명(컬럼1 자료형 키설정정, 컬럼2 자료형, 컬럼3 자료형)
+create table author(id int primary key, name varchar(255), email varchar(255), password varchar(255));
+
+-- 테이블 목록 조회
+show tables;
+
+-- 테이블 컬럼 정보 조회
+describe 테이블명;
+describe author;
+
+-- 테이블 생성 명령문 조회
+show create table 테이블명;
+
+-- Ex> posts 테이블 신규 생성(id, title, contents, author_id) 
+-- + 다른 방법으로 키 지정 : primary key(id), foreign key(author_id)
+-- 외래키와 연결할 테이블(컬럼) 지정 : references 테이블명(컬럼)
+create table posts(id int, title varchar(255), contents varchar(255), author_id int not null, primary key(id), foreign key(author_id) references author(id));
+
+-- 테이블 제약조건 조회
+select * from information_schema.key_column_usage where table_name='posts';
+
+-- 테이블 index 조회
+show index from author;
+
+-- alter : 테이블의 구조를 변경
+-- 테이블의 이름 변경
+alter table posts rename post;
+-- 테이블의 컬럼 추가
+alter table author add column age int;
+-- 테이블 컬럼 삭제
+alter table author drop column age;
+-- 테이블 컬럼명 변경
+alter table post change column contents content;
+-- 테이블 컬럼의 타입과 제약조건 변경 => 덮어쓰기
+alter table author modify column email varchar(100) not null;
+alter table author modify column email varchar(100) not null unique;    -- 제약조건 덮어쓰기
+
+-- 실습 : author 테이블에 address 컬럼을 추가(varchar(255))
+alter table author add column address varchar(255);
+-- 실습 : post 테이블에 title 은 not null로 변경, content는 길이 3000자로 변경
+alter table post modify column title varchar(255) not null, modify column content varchar(3000);
+
+-- drop : 테이블을 삭제하는 명령어
+drop table post; -- 가능
+drop table author -- 불가능 -> foreign key 때문
+--일련의 쿼리를 실행시킬 때 특정 쿼리에서 에러가 나지 않도록 if exists를 많이 사용
+drop table if exists abc;   -- 존재한다면 삭제한다는 의미, 없어도 에러 안 나고 경고만 함
---
+++ 2.DML.sql
@@ -0,0 +1,55 @@
+-- insert : 테이블에 데이터 삽입
+insert into 테이블명(컬럼1, 컬럼2, 컬럼3) values(데이터1, 데이터2, 데이터3);
+insert into author(id, name, email) values(3, 'hong3', 'hong3@naver.com'); -- 문자열
+-- update : 테이블에 데이터 변경
+update author set name="홍길동", email="hong100@naver.com" where id=3;
+
+-- select : 조회
+select 컬럼1, 컬럼2 from 테이블명;
+select name, email from author;
+select * from author;
+
+-- delete : 삭제
+delete from 테이블명 where 조건절;
+delete from author where id=3;
+
+-- select 조건절 활용 조회
+-- 테스트 데이터 삽입
+-- insert 문을 활용하여 author 데이터 3개, post 데이터 5개 추가
+select * from author;   -- 어떤 조회조건 없이 모든 컬럼 조회
+select * from author where id=1;    -- where 뒤에 조회조건을 통해 filtering
+select * from author where name='hong1';    -- 이름 조회
+select * from author where id>3;    -- id가 4번 이상인 id 조회
+select * from author where id>2 and name='hong4'; -- id가 2 이상이면서 name이 hong4 조회
+
+-- 중복제거 조회 : distinct
+select name from author;
+select distinct name from author;
+
+-- 정렬 : order by + 컬럼명
+-- asc : 오름차순, desc : 내림차순, 안붙이면 오름차순(default)
+-- 아무런 정렬조건 없이 조회할 경우에는 pk 기준으로 오름차순
+select * from author order by name;
+select * from author order by name desc;    -- 내림차순
+
+-- 멀티컬럼 order by : 여러 컬럼으로 정렬시, 먼저 쓴 컬럼 우선정렬.
+-- 중복시, 그 다음 정렬 옵션 적용
+select * from author order by name desc, email asc; --name으로 정렬 후, name이 중복되면 email로 정렬
+
+-- 결과값 개수 제한
+select * from author order by id desc limit 1;  -- 최신순으로 생성된 1개의 id 조회
+
+-- 별칭(alias)를 이용한 select
+select name as '이름', email as '이메일' from author;   -- name은 '이름', email은 '이메일'로 별칭 생성성
+select a.name, a.email from author as a;    -- a.name, a.email 로 별칭 생성
+select a.name, a.email from author a;   -- as 생략 가능
+
+-- null을 조회조건으로 활용
+select * from author where password is null;
+select * from author where password is not null;
+
+-- 프로그래머스 sql 문제풀이
+-- 여러 기준으로 정렬하기
+SELECT ANIMAL_ID, NAME, DATETIME FROM ANIMAL_INS ORDER BY NAME, DATETIME DESC;
+-- 상위 n개 레코드
+SELECT NAME FROM ANIMAL_INS ORDER BY DATETIME LIMIT 1;
